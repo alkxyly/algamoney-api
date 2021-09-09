f8f6b8201b2a20ce16274efc331e4c48dece8d92
alkxyly <alkxyly@hotmail.com>
2021-09-09 17:33:58 -0300

    3.6. Cadastrando nova categoria com POST

--- src/main/java/com/algaworks/algamoney/api/controller/CategoriaController.java
+++ src/main/java/com/algaworks/algamoney/api/controller/CategoriaController.java
@@ -1,11 +1,20 @@
 package com.algaworks.algamoney.api.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoney.api.model.Categoria;
 import com.algaworks.algamoney.api.repository.CategoriaRepository;
@@ -21,5 +30,22 @@ public class CategoriaController {
 	@GetMapping
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse httpServletResponse) {
+		Categoria categoriaCriada = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaCriada.getCodigo())
+				.toUri();
+		httpServletResponse.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaCriada);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscaPeloCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findById(codigo).get();
 	}
 }
