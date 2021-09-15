eabb692205087b30eaf0a6086866b991270d88a3
alkxyly <alkxyly@hotmail.com>
2021-09-14 23:00:11 -0300

    4.3. Atualizando pessoa com PUT

--- src/main/java/com/algaworks/algamoney/api/controller/PessoaController.java
+++ src/main/java/com/algaworks/algamoney/api/controller/PessoaController.java
@@ -7,14 +7,18 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,6 +28,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.algaworks.algamoney.api.event.RecursoCriadoEvent;
 import com.algaworks.algamoney.api.model.Pessoa;
 import com.algaworks.algamoney.api.repository.PessoaRepository;
+import com.algaworks.algamoney.api.service.PessoaService;
 
 @RestController
 @RequestMapping(path = "/pessoas")
@@ -34,6 +39,9 @@ public class PessoaController {
 	
 	@Autowired
 	private ApplicationEventPublisher eventPublisher;
+	
+	@Autowired
+	private PessoaService pessoaService;
 	
 	@GetMapping
 	public List<Pessoa> listar(){
@@ -58,5 +66,11 @@ public class PessoaController {
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void deletar(@PathVariable Long codigo) {
 		pessoaRepository.deleteById(codigo);
+ 	}
+	
+	@PutMapping("/{codigo}/endereco")
+	public ResponseEntity<Object> atualizar(@RequestBody @Valid Pessoa pessoa, @PathVariable Long codigo){
+		Pessoa pessoaSalva = pessoaService.atualizar(pessoa, codigo);
+		return ResponseEntity.status(HttpStatus.OK).body(pessoaSalva);
 	}
 }
