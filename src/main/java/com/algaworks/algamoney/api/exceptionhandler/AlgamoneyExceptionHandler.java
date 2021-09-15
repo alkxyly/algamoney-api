17070e136c763fbd674711164ae9395350c871ea
alkxyly <alkxyly@hotmail.com>
2021-09-14 22:03:05 -0300

    4.1. Removendo pessoa com DELETE

--- src/main/java/com/algaworks/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/algaworks/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -46,6 +48,16 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		var errors = criarListaErro(ex.getBindingResult());
 		
 		return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+		String mensagemInvalida = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		String mensagemDesenv = ex.toString();
+	
+		var errors = Arrays.asList(new Erro(mensagemInvalida, mensagemDesenv));
+		
+		return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 	
 	private List<Erro> criarListaErro(BindingResult bindingResult){
--- src/main/java/com/algaworks/algamoney/api/model/Endereco.java
+++ src/main/java/com/algaworks/algamoney/api/model/Endereco.java
@@ -1,7 +1,6 @@
 package com.algaworks.algamoney.api.model;
 
 import javax.persistence.Embeddable;
-import javax.validation.constraints.NotBlank;
 
 import lombok.Data;
 
@@ -9,16 +8,11 @@ import lombok.Data;
 @Data
 public class Endereco {
 
-	@NotBlank
 	private String logradouro;
-	@NotBlank
 	private String numero;
 	private String complemento;
-	@NotBlank
 	private String bairro;
 	private String cep;
-	@NotBlank
 	private String cidade;
-	@NotBlank
 	private String estado;
 }
