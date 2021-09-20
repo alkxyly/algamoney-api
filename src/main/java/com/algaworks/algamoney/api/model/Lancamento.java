8133447fe65b04e36c62e6b43cd6837eb72b2c49
alkxyly <alkxyly@hotmail.com>
2021-09-19 22:00:45 -0300

    5.4. Validando inconsistências

--- src/main/java/com/algaworks/algamoney/api/model/Lancamento.java
+++ src/main/java/com/algaworks/algamoney/api/model/Lancamento.java
@@ -14,8 +14,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import lombok.Data;
 
 @Entity
