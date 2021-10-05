57f82936937faf8e39bee16295feac449fa891df
alkxyly <alkxyly@hotmail.com>
2021-10-05 12:15:22 -0300

    7.5. Nome do usuário no token JWT

--- src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.algaworks.algamoney.api.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,10 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import com.algaworks.algamoney.api.config.token.CustomTokenEnhancer;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -47,25 +53,30 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+		
 		endpoints
-		.authenticationManager(authenticationManager)
-		.userDetailsService(userDetailsService)
-		.accessTokenConverter(accessTokenConverter())
-		.tokenStore(tokenStore())
-		.reuseRefreshTokens(false);
+			.tokenStore(tokenStore())
+			.tokenEnhancer(tokenEnhancerChain)
+			.reuseRefreshTokens(false)
+			.authenticationManager(authenticationManager);
 	}
-
+	
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-
 		accessTokenConverter.setSigningKey("3032885ba9cd6621bcc4e7d6b6c35c2b");
-
 		return accessTokenConverter;
 	}
 
 	@Bean
 	public TokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
+	}
+	
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+	    return new CustomTokenEnhancer();
 	}
 }
--- src/main/java/com/algaworks/algamoney/api/security/AppUserDetailsService.java
+++ src/main/java/com/algaworks/algamoney/api/security/AppUserDetailsService.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,22 +20,18 @@ import com.algaworks.algamoney.api.repository.UsuarioRepository;
 public class AppUserDetailsService implements UserDetailsService{
 
 	@Autowired
-	private UsuarioRepository usuarioReposity;
+	private UsuarioRepository usuarioRepository;
 	
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		Optional<Usuario> usuarioOp = usuarioReposity.findByEmail(email);
-		Usuario usuario = usuarioOp.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorreto"));
-		return new User(email, usuario.getSenha(), getPermissoes(usuario));
+		Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
+		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
+		return new UsuarioSistema(usuario, getPermissoes(usuario));
 	}
 
 	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
-		Set<SimpleGrantedAuthority> authorities	= new HashSet<>();
-		
-		usuario.getPermissoes().forEach(permissao -> 
-			authorities.add(new SimpleGrantedAuthority(permissao.getDescricao().toUpperCase())));
-		
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+		usuario.getPermissoes().forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getDescricao().toUpperCase())));
 		return authorities;
 	}
-
 }
