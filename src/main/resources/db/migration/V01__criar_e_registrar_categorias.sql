3aeea0926b13a84129d97b4967acaa8adadabc0e
alkxyly <alkxyly@hotmail.com>
2021-10-08 12:08:56 -0300

    +1 categoria

--- src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -7,4 +7,5 @@ INSERT INTO categoria (nome) values ('Lazer');
 INSERT INTO categoria (nome) values ('Alimentação');
 INSERT INTO categoria (nome) values ('Supermercado');
 INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Estudos');
 INSERT INTO categoria (nome) values ('Outros');
