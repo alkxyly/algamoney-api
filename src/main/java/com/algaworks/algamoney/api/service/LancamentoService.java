e4117a00a637c595c9049e257879b8c25db78385
alkxyly <alkxyly@hotmail.com>
2021-10-14 23:24:58 -0300

    7.9. Desafio: Atualização de lançamento

--- src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -27,13 +28,44 @@ public class LancamentoService {
 		return lancamentoRepository.findById(codigo)
 				.orElseThrow(() ->  new EmptyResultDataAccessException(1));
 	}
-
-
+	
 	public Lancamento salvar(@Valid Lancamento lancamento) {
 		Optional<Pessoa> pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
 		if(!pessoa.isPresent() || pessoa.get().isInativo())
 			throw new PessoaInexistenteOuInativaException();
 			
 		return lancamentoRepository.save(lancamento);
+	}
+	
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+	
+	private void validarPessoa(Lancamento lancamento) {
+		Pessoa pessoa = null;
+		if (lancamento.getPessoa().getCodigo() != null) {
+			pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo())
+								     .orElseThrow(() -> new PessoaInexistenteOuInativaException());
+		}
+
+		if (pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+	}
+
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		Lancamento lancamentoSalvo = lancamentoRepository.findById(codigo)
+							.orElseThrow(() -> new PessoaInexistenteOuInativaException());
+		if (lancamentoSalvo == null) {
+			throw new IllegalArgumentException();
+		}
+		return lancamentoSalvo;
 	}
 }
